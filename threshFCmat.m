506df91be17dab26f2dbcb99d8e458acdae01791
dawnsong <dawnwei.song@gmail.com>
2013-07-06 13:16:58 -0700

    wrap threshFCmat

--- threshFCmat.m
+++ threshFCmat.m
@@ -1,5 +1,10 @@
 function threshFCmat(afcmat, athdprfx, ap)
-load afcmat;
+%
+%
+% Copyright (C) 2013 Xiaowei.Song <dawnwei.song@gmail.com>
+% Distributed under terms of the AFL (Academy Free license).
+%
+eval(sprintf('load %s', afcmat));
 
 msk= p>=ap;
 fcmat(~msk)=0;
@@ -7,23 +12,23 @@ fcmat(~msk)=0;
 %positive
 pfcmat=fcmat;
 pm= fcmat>0;
-pfcmat[~pm]=0;
+pfcmat(~pm)=0;
 
 [i,j,s]=find(pfcmat);
 [m,n]=size(pfcmat);
 sfcmat=sparse(i,j,s,m,n);
-fn=sprintf("%s.Pge%g.positive.mat",athdprfx,ap);
+fn=sprintf('%s.Pge%g.positive.mat',athdprfx,ap);
 save(fn, 'sfcmat')
 clear pfcmat pm sfcmat;
 
 %negative
 nfcmat=fcmat;
 nm= fcmat<0;
-nfcmat[~nm]=0;
+nfcmat(~nm)=0;
 
 [i,j,s]=find(nfcmat);
 [m,n]=size(nfcmat);
 sfcmat=sparse(i,j,s,m,n);
-fn=sprintf("%s.Pge%g.negative.mat",athdprfx,ap);
+fn=sprintf('%s.Pge%g.negative.mat',athdprfx,ap);
 save(fn, 'sfcmat')
 clear nfcmat nm sfcmat;
---
+++ threshFCmat.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#
+# threshFCmat.sh
+#% Init: 2013-07-06 12:38
+#% Copyright (C) 2013~2020 Xiaowei.Song <dawnwei.song@gmail.com>
+#% Distributed under terms of the AFL (Academy Free license).
+#
+elog(){ echo "$@" 1>&2 ; }
+if [ $# -eq 0 ]; then
+  cat <<eof
+Usage: $0 fcmat thrd.prfx 0.0001=p
+eof
+  exit 1
+fi 
+
+CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+matlabg <<eof
+addpath(genpath('$CALLDIR'));
+threshFCmat('$1','$2',$3);
+eof
+
+
