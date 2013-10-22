53b80becdce8979387b01117f4fa600893c68659
dawnsong <dawnsong.tsinghua@gmail.com>
2013-10-22 12:18:29 -0400

    delete obsolete fcmat2nii

--- fcmat2nii.m
+++
@@ -1,9 +0,0 @@
-function [result] = fcmat2nii(fcmat, fnii)
-%mat2nii:
-%
-%Copyright (C) 2013 Xiao-Wei Song <dawnwei.song@gmail.com>
-
-load(fcmat);
-img=zeros([size(fcmat), 2]);
-nii=make_nii(img);
-save_nii(nii, fnii);
